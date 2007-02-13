3cd7b5dadb791d9f69c3bb7ba4cc99be66e70225
Damian Flannery <d.w.flannery@gmail.com>
2007-02-13 09:50:08 +0000

    Added GateKeeper service to check if a given user has permission to perform a specific operation on a particular object of a particular type e.g. dwf64 perform read operation on Investigation #23456.

---
+++ icat3-core/src/uk/icat3/security/GateKeeper.java
@@ -0,0 +1,24 @@
+/*
+ * GateKeeper.java
+ *
+ * Created on 13 February 2007, 08:41
+ *
+ * To change this template, choose Tools | Template Manager
+ * and open the template in the editor.
+ */
+
+package uk.icat3.security;
+
+/**
+ *
+ * @author df01
+ */
+public class GateKeeper {
+    
+    /** Creates a new instance of GateKeeper */
+    public GateKeeper() {
+    }
+    
+    
+    
+}
