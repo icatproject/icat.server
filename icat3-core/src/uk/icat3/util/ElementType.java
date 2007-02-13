d95b877e60afdce07c60f55912d3df2904d63205
Damian Flannery <d.w.flannery@gmail.com>
2007-02-13 09:47:16 +0000

    Added ElementType to complement AccessType for use in GateKeeper (Security check) service

---
+++ icat3-core/src/uk/icat3/util/ElementType.java
@@ -0,0 +1,18 @@
+/*
+ * ElementType.java
+ *
+ * Created on 13 February 2007, 09:43
+ *
+ * To change this template, choose Tools | Template Manager
+ * and open the template in the editor.
+ */
+
+package uk.icat3.util;
+
+/**
+ *
+ * @author df01
+ */
+public enum ElementType {
+    STUDY, INVESTIGATION, DATASET, DATAFILE, DATAFILE_PARAMETER, DATASET_PARAMETER, SAMPLE, SAMPLE_PARAMETER;
+}
