5e8b00a89a467accd22e30664b5348ed1e6b13e3
Damian Flannery <d.w.flannery@gmail.com>
2007-02-21 09:43:51 +0000

    Added Javadoc comments

--- icat3-core/src/uk/icat3/util/ElementType.java
+++ icat3-core/src/uk/icat3/util/ElementType.java
@@ -1,17 +1,18 @@
+package uk.icat3.util;
+
 /*
  * ElementType.java
  *
  * Created on 13 February 2007, 09:43
  *
- * To change this template, choose Tools | Template Manager
- * and open the template in the editor.
- */
-
-package uk.icat3.util;
-
-/**
+ * ElementType is simply an enum (data structure whose fields 
+ * consist of a fixed set of constants) that contains the possible
+ * elements / entities of the icat3 model that a user may wish to
+ * interact with.  This enum is used in the GateKeeper (Security,
+ * authorisation) service.
  *
  * @author df01
+ * @version 1.0
  */
 public enum ElementType {
     STUDY, INVESTIGATION, DATASET, DATAFILE, DATAFILE_PARAMETER, DATASET_PARAMETER, SAMPLE, SAMPLE_PARAMETER;
