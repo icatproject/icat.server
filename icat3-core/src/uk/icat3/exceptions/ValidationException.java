cb3dfddb999fa6d3127048bb48541bbdc9e35c45
Glen Drinkwater <someone@example.com>
2007-03-08 17:52:56 +0000

    ""

---
+++ icat3-core/src/uk/icat3/exceptions/ValidationException.java
@@ -0,0 +1,33 @@
+/*
+ * ValidationException.java
+ *
+ * Created on 08 March 2007, 12:24
+ *
+ * To change this template, choose Tools | Template Manager
+ * and open the template in the editor.
+ */
+
+package uk.icat3.exceptions;
+
+/**
+ * 
+ *
+ * @author gjd37
+ */
+public class ValidationException extends ICATAPIException {
+    
+    /**
+     * Creates a new instance of <code>ValidationException</code> without detail message.
+     */
+    public ValidationException() {
+    }
+    
+    
+    /**
+     * Constructs an instance of <code>ValidationException</code> with the specified detail message.
+     * @param msg the detail message.
+     */
+    public ValidationException(String msg) {
+        super(msg);
+    }
+}
