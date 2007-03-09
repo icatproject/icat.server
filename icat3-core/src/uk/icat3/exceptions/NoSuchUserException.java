c31e2905c7013dd3c6d241ce4d72aef7d7ec73e7
Damian Flannery <d.w.flannery@gmail.com>
2007-03-09 15:17:02 +0000

    Added exceptions back into project

---
+++ icat3-core/src/uk/icat3/exceptions/NoSuchUserException.java
@@ -0,0 +1,32 @@
+/*
+ * NoSuchUserException.java
+ *
+ * Created on 21 February 2007, 14:45
+ *
+ * To change this template, choose Tools | Template Manager
+ * and open the template in the editor.
+ */
+
+package uk.icat3.exceptions;
+
+/**
+ *
+ * @author df01
+ */
+public class NoSuchUserException extends ICATAPIException {
+    
+    /**
+     * Creates a new instance of <code>NoSuchUserException</code> without detail message.
+     */
+    public NoSuchUserException() {
+    }
+    
+    
+    /**
+     * Constructs an instance of <code>NoSuchUserException</code> with the specified detail message.
+     * @param msg the detail message.
+     */
+    public NoSuchUserException(String msg) {
+        super(msg);
+    }
+}
