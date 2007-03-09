67ef2aa6490e0bb73bc66a54128ccd3388a48531
Damian Flannery <d.w.flannery@gmail.com>
2007-03-09 11:42:14 +0000

    Moved core userdb classes/interfaces and exceptions to icat core project

---
+++ icat3-core/src/uk/icat3/user/User.java
@@ -0,0 +1,84 @@
+package uk.icat3.user;
+
+import uk.icat3.user.exceptions.LoginException;
+import uk.icat3.user.exceptions.NoSuchUserException;
+
+/*
+ * User.java
+ *
+ * Created on 20 February 2007, 15:52
+ *
+ * The icat3 api enables each facility to plug in their own
+ * user database to authenticate and provide personal details
+ * such as name, title, email address etc. to the api callers
+ * as context information e.g. displaying names of investigators
+ * for an experiment instead of just their userid/distinguished
+ * name as held in the icat3 database.
+ *
+ * <p>In order to acheive this, the icat2 api provides a contract
+ * (java interface) of methods that any user database (to be used
+ * by icat3) must implement.  These methods and their signatures
+ * are defined below.</p> 
+ *
+ * @author df01
+ * @version 1.0
+ */
+public interface User {
+    
+    /**
+     * Returns a userid/distinguished name for a user with a valid
+     * sessionId.  If user does not have a valid sessionId then an
+     * {@link LoginException} will be thrown.
+     *
+     * @param sessionId authentication token obtained on successful login
+     * @return userid/distinguished name of user
+     * @throws LoginException  if user provides an invalid sessionId
+     */
+    public String getUserIdFromSessionId (String sessionId) throws LoginException;
+    
+    /**
+     * Returns a sessionId (authenitcation token) to a user after verification
+     * of correct username and password combination.  If user does not provide 
+     * the correct login credentials a {@link LoginException} will be thrown.
+     *
+     * @param username username/dn of user
+     * @param password of user
+     * @return sessionId authentication token that user can use in corresponding
+     *                   methods calls without having to provide username and
+     *                   password each time.
+     * @throws LoginException   if user provides an invalid username and password
+     *                          combination.
+     */
+    public String login (String username, String password) throws LoginException;
+    
+    /**
+     * Removes sessionId authentication token from user database which
+     * effectively logs user out of the system.  Any further attempt to
+     * use the icat3 api with an 'old' sessionId will fail as it will
+     * no longer exist in the user database.  Any potential exceptions
+     * that could be raised should be suppressed e.g. logging out a user
+     * who is already logged out.
+     *
+     * @param sessionId authentication token obtained on successful login     
+     */
+    public void logout (String sessionId);
+    
+    /**
+     * Returns a serializable object that contains personal details of a requested
+     * <code>user</code> as defined in {@link UserDetails}.  A {@link LoginException}
+     * should be thrown if an invalid sessionId is supplied and a 
+     * {@link No SuchUserException} should be thrown if the requested <code>user</code>
+     * cannot be found in the database.  If both <code>sessionId</code> and 
+     * <code>user</code> are invalid then a {@link LoginException} should be thrown first.
+     *
+     * @param username username/dn of user
+     * @param password of user
+     * @return sessionId authentication token that user can use in corresponding
+     *                   methods calls without having to provide username and
+     *                   password each time.
+     * @throws LoginException   if user provides an invalid <code>sessionId</code>
+     * @throws NoSuchUserException   if <code>user</code> cannot be found in user database
+     * @see UserDetails
+     */
+    public UserDetails getUserDetails(String sessionId, String user) throws LoginException, NoSuchUserException;     
+}
---
+++ icat3-core/src/uk/icat3/user/UserManager.java
@@ -0,0 +1,63 @@
+package uk.icat3.user;
+
+import uk.icat3.user.*;
+import uk.icat3.user.exceptions.LoginException;
+import uk.icat3.user.exceptions.NoSuchUserException;
+
+
+/*
+ * UserManager.java
+ *
+ * Created on 20 February 2007, 15:53
+ *
+ * Added UserManager class that will act as the user database implementation 
+ * and will be used directly by icat3.  In fact, this class will delegate all 
+ * method calls to a facility specific implementation of the User.java interface 
+ * (e.g. to ISISUser.java - which will provide all the necessary work to 
+ * retrieve the desired information from the ISIS user database and present it 
+ * in a form digestable for icat3).
+ * 
+ * @author df01
+ * @version 1.0
+ */
+public class UserManager implements User {        
+    private User user;
+    
+    /** Creates a new instance of UserManager */
+    public UserManager() throws LoginException {
+        user = (uk.icat3.user.User) createObject("uk.icat3.user.ISISUser");     
+    }
+    
+    public String getUserIdFromSessionId (String sessionId) throws LoginException {
+        return user.getUserIdFromSessionId(sessionId);
+    }
+    
+    public String login (String username, String password) throws LoginException {
+        return user.login(username, password);
+    }
+    
+    public void logout (String sessionId) {
+        user.logout(sessionId);
+    }
+    
+    public UserDetails getUserDetails(String sessionId, String user) throws LoginException, NoSuchUserException {
+        return this.user.getUserDetails(sessionId, user);
+    }
+    
+    static Object createObject(String className) {
+      Object object = null;
+      try {
+          Class classDefinition = Class.forName(className);
+          object = classDefinition.newInstance();
+      } catch (InstantiationException e) {
+          System.out.println(e);
+      } catch (IllegalAccessException e) {
+          System.out.println(e);
+      } catch (ClassNotFoundException e) {
+          System.out.println(e);
+      }
+      return object;
+   }
+    
+}
+
