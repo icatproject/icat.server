b70eec3ccb0369b20722f081e4902c8ed860e715
Glen Drinkwater <someone@example.com>
2007-06-04 09:47:21 +0000

    unit testing

--- icat3-exposed/test/uk/icat3/exposed/TestAll.java
+++ icat3-exposed/test/uk/icat3/exposed/TestAll.java
@@ -3,11 +3,31 @@ import junit.framework.JUnit4TestAdapter;
 import junit.framework.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
+import uk.icat3.exposed.investigationmanager.TestInvestigation;
+import uk.icat3.exposed.investigationmanager.TestInvestigator;
+import uk.icat3.exposed.investigationmanager.TestKeyword;
+import uk.icat3.exposed.investigationmanager.TestPublication;
+import uk.icat3.exposed.investigationmanager.TestSample;
+import uk.icat3.exposed.investigationmanager.TestSampleParameter;
 
 
 @RunWith(Suite.class)
 @Suite.SuiteClasses({
-   
+            TestSample.class,
+            TestKeyword.class,
+            TestPublication.class,
+            TestInvestigator.class,
+            TestInvestigation.class,
+            TestSampleParameter.class
+            
+           // TestDataset.class,
+            //TestDatasetParameter.class,
+            
+          //  TestDatafile.class,
+           // TestDatafileParameter.class,
+            
+          //  TestKeywordSearch.class,
+           // TestInvalidUser.class
 })
 public class TestAll {
        
