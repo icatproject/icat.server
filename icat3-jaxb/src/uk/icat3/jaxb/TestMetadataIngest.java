60b86c348185537046dc91150dfc7037264a8c95
Damian Flannery <d.w.flannery@gmail.com>
2007-06-13 14:16:44 +0000

    Code to test ingest xml into ICAT3

---
+++ icat3-jaxb/src/uk/icat3/jaxb/TestMetadataIngest.java
@@ -0,0 +1,59 @@
+/*
+ * TestMetadataIngest.java
+ * 
+ * Created on 13-Jun-2007, 10:01:15
+ * 
+ * To change this template, choose Tools | Template Manager
+ * and open the template in the editor.
+ */
+
+package uk.icat3.jaxb;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+/**
+ *
+ * @author df01
+ */
+public class TestMetadataIngest {
+
+    public TestMetadataIngest() {
+    }
+    
+    public static void main (String[] args) {
+        EntityManagerFactory emf =  Persistence.createEntityManagerFactory("icat3-jaxbPU");
+        EntityManager em = emf.createEntityManager();
+        
+        em.getTransaction().begin();
+                
+
+try {
+            BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream("src/uk/icat3/jaxb/icatXML.xml")));
+            String line = "";
+            String buffer = "";
+            while((line = br.readLine()) != null) {
+            buffer += line;
+            }
+            System.out.println("buffer: " + buffer);           
+        
+            MetadataIngest.ingestMetadata("dwf64", buffer, em);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+
+        em.getTransaction().commit();
+        
+        em.close();
+        
+    }
+
+   
+
+}
