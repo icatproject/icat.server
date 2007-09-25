68419f0da88294f24bd247645a2720860007cc44
Glen Drinkwater <someone@example.com>
2007-09-25 11:02:07 +0000

    changed exceptions to ICATAPIExceptions

--- icat3-ws-test/src/icat3wstest/Constants.java
+++ icat3-ws-test/src/icat3wstest/Constants.java
@@ -15,11 +15,11 @@ package icat3wstest;
  */
 public class Constants {
     
-   public static String END_POINT_ADDRESS = "http://facilities01.esc.rl.ac.uk:8081/ICATISISService/ICAT";
+  // public static String END_POINT_ADDRESS = "http://facilities01.esc.rl.ac.uk:8081/ICATISISService/ICAT";
   
-   //  public static String END_POINT_ADDRESS = "https://tiber.dl.ac.uk:8182/ICATCLFService/ICAT";
+    public static String END_POINT_ADDRESS = "https://escvig6.dl.ac.uk:8181/ICATService/ICAT";
     //public static String SID = "f20cb521-73cd-4f52-b379-f66ad8f9529f"; //auth2
-     public static String SID = "d8e70ddf-b01e-4c75-89a5-b425d3d8df73";
+     public static String SID = "6f24f8b3-a73e-44df-bfe6-926c9b019a16";
      
     public static String KEYWORD = "calibration";
     public static String USER_ID = "gjd37";
