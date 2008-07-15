573ac0091909f424eb9656bb98e3384e7c0336a6
Damian Flannery <d.w.flannery@gmail.com>
2008-07-15 16:39:35 +0000

    Added class for F5 tests

---
+++ icat3-acc-tests/test/uk/icat3/acctests/functional/ICAT_F_5.java
@@ -0,0 +1,46 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package uk.icat3.acctests.functional;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author df01
+ */
+public class ICAT_F_5 {
+
+    public ICAT_F_5() {
+    }
+
+    @BeforeClass
+    public static void setUpClass() throws Exception {
+    }
+
+    @AfterClass
+    public static void tearDownClass() throws Exception {
+    }
+
+    @Before
+    public void setUp() {
+    }
+
+    @After
+    public void tearDown() {
+    }
+
+    // TODO add test methods here.
+    // The methods must be annotated with annotation @Test. For example:
+    //
+    // @Test
+    // public void hello() {}
+
+}
