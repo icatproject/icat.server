a8c1c643b5e11d881e009f688aa5ebcd05109e1a
Damian Flannery <d.w.flannery@gmail.com>
2008-07-15 16:36:20 +0000

    Added more tests

--- icat3-acc-tests/test/uk/icat3/acctests/functional/ICAT_F_2.java
+++ icat3-acc-tests/test/uk/icat3/acctests/functional/ICAT_F_2.java
@@ -13,9 +13,10 @@ import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
-import uk.icat3.acctests.util.Constants;
+import uk.icat3.acctests.util.Helper;
+import static uk.icat3.acctests.util.Constants.*;
 import static org.junit.Assert.*;
-
+import static uk.icat3.client.InvestigationInclude.*;
 /**
  *
  * @author df01
@@ -40,16 +41,16 @@ public class ICAT_F_2 {
             adminService = new uk.icat3.client.admin.ICATAdminService();
             adminPort = adminService.getICATAdminPort();
             
-            ((BindingProvider)adminPort).getRequestContext().put(BindingProvider.USERNAME_PROPERTY, Constants.ICAT_ADMIN_USER);
-            ((BindingProvider)adminPort).getRequestContext().put(BindingProvider.PASSWORD_PROPERTY, Constants.ICAT_ADMIN_PASSWORD);  
+            ((BindingProvider)adminPort).getRequestContext().put(BindingProvider.USERNAME_PROPERTY, ICAT_ADMIN_USER);
+            ((BindingProvider)adminPort).getRequestContext().put(BindingProvider.PASSWORD_PROPERTY, ICAT_ADMIN_PASSWORD);  
             
             service = new uk.icat3.client.ICATService();
             port = service.getICATPort();
             
             keywords = new ArrayList<String>();            
-            keywords.add(Constants.ICAT_F_2_KEYWORD1);
-            keywords.add(Constants.ICAT_F_2_KEYWORD2);
-            keywords.add(Constants.ICAT_F_2_KEYWORD3);
+            keywords.add(ICAT_F_2_KEYWORD1);
+            keywords.add(ICAT_F_2_KEYWORD2);
+            keywords.add(ICAT_F_2_KEYWORD3);
             
                         
         } catch (Exception ex) {
@@ -87,16 +88,332 @@ public class ICAT_F_2 {
             //get armstrong investigation
             List<uk.icat3.client.Investigation> investigations = port.searchByKeywords(sessionId, keywords);        
             
+            //if no results returned, don't bother do anything else
             if (investigations.size() == 0) assertTrue(false);
-            uk.icat3.client.Investigation i = port.getInvestigationIncludes(sessionId, investigations.get(0).getId(), uk.icat3.client.InvestigationInclude.ALL);
             
+            uk.icat3.client.Investigation i = port.getInvestigationIncludes(sessionId, investigations.get(0).getId(), ALL);            
             System.out.println("name: " + i.getTitle());            
-            assertTrue(investigations.size() > 0);
-                      
             
+            //check that all requested objects are contained within search result
+            Helper.checkIncluded(i, ALL);
+            
+            //if we get here without exception then all is OK!
+            assertTrue(true);
+                                  
         } catch (Exception ex) {
             ex.printStackTrace();
         }
     }
 
+    
+    @Test
+    public void keywordSearchAllExceptDatasetsAndDatafiles() {    
+        try {  
+            // TODO process result here
+            sessionId = adminPort.loginAdmin(USER1);            
+            if (sessionId == null) assertTrue(false);                        
+            System.out.println("sessionId: " + sessionId);
+                                    
+            //get armstrong investigation
+            List<uk.icat3.client.Investigation> investigations = port.searchByKeywords(sessionId, keywords);        
+            
+            //if no results returned, don't bother do anything else
+            if (investigations.size() == 0) assertTrue(false);
+            
+            uk.icat3.client.Investigation i = port.getInvestigationIncludes(sessionId, investigations.get(0).getId(), ALL_EXCEPT_DATASETS_AND_DATAFILES);            
+            System.out.println("name: " + i.getTitle());            
+            
+            //check that all requested objects are contained within search result
+            Helper.checkIncluded(i, ALL_EXCEPT_DATASETS_AND_DATAFILES);
+            
+            //if we get here without exception then all is OK!
+            assertTrue(true);
+                                  
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+    
+    @Test
+    public void keywordSearchDatasetsAndDatafiles() {    
+        try {  
+            // TODO process result here
+            sessionId = adminPort.loginAdmin(USER1);            
+            if (sessionId == null) assertTrue(false);                        
+            System.out.println("sessionId: " + sessionId);
+                                    
+            //get armstrong investigation
+            List<uk.icat3.client.Investigation> investigations = port.searchByKeywords(sessionId, keywords);        
+            
+            //if no results returned, don't bother do anything else
+            if (investigations.size() == 0) assertTrue(false);
+            
+            uk.icat3.client.Investigation i = port.getInvestigationIncludes(sessionId, investigations.get(0).getId(), DATASETS_AND_DATAFILES);            
+            System.out.println("name: " + i.getTitle());            
+            
+            //check that all requested objects are contained within search result
+            Helper.checkIncluded(i, DATASETS_AND_DATAFILES);
+            
+            //if we get here without exception then all is OK!
+            assertTrue(true);
+                                  
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+    
+    @Test
+    public void keywordSearchDatasetsDatafilesAndParameters() {    
+        try {  
+            // TODO process result here
+            sessionId = adminPort.loginAdmin(USER1);            
+            if (sessionId == null) assertTrue(false);                        
+            System.out.println("sessionId: " + sessionId);
+                                    
+            //get armstrong investigation
+            List<uk.icat3.client.Investigation> investigations = port.searchByKeywords(sessionId, keywords);        
+            
+            //if no results returned, don't bother do anything else
+            if (investigations.size() == 0) assertTrue(false);
+            
+            uk.icat3.client.Investigation i = port.getInvestigationIncludes(sessionId, investigations.get(0).getId(), DATASETS_DATAFILES_AND_PARAMETERS);            
+            System.out.println("name: " + i.getTitle());            
+            
+            //check that all requested objects are contained within search result
+            Helper.checkIncluded(i, DATASETS_DATAFILES_AND_PARAMETERS);
+            
+            //if we get here without exception then all is OK!
+            assertTrue(true);
+                                  
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+    
+    @Test
+    public void keywordSearchDatasetsOnly() {    
+        try {  
+            // TODO process result here
+            sessionId = adminPort.loginAdmin(USER1);            
+            if (sessionId == null) assertTrue(false);                        
+            System.out.println("sessionId: " + sessionId);
+                                    
+            //get armstrong investigation
+            List<uk.icat3.client.Investigation> investigations = port.searchByKeywords(sessionId, keywords);        
+            
+            //if no results returned, don't bother do anything else
+            if (investigations.size() == 0) assertTrue(false);
+            
+            uk.icat3.client.Investigation i = port.getInvestigationIncludes(sessionId, investigations.get(0).getId(), DATASETS_ONLY);            
+            System.out.println("name: " + i.getTitle());            
+            
+            //check that all requested objects are contained within search result
+            Helper.checkIncluded(i, DATASETS_ONLY);
+            
+            //if we get here without exception then all is OK!
+            assertTrue(true);
+                                  
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+    
+    @Test
+    public void keywordSearchInvestigatorsAndKeywords() {    
+        try {  
+            // TODO process result here
+            sessionId = adminPort.loginAdmin(USER1);            
+            if (sessionId == null) assertTrue(false);                        
+            System.out.println("sessionId: " + sessionId);
+                                    
+            //get armstrong investigation
+            List<uk.icat3.client.Investigation> investigations = port.searchByKeywords(sessionId, keywords);        
+            
+            //if no results returned, don't bother do anything else
+            if (investigations.size() == 0) assertTrue(false);
+            
+            uk.icat3.client.Investigation i = port.getInvestigationIncludes(sessionId, investigations.get(0).getId(), INVESTIGATORS_AND_KEYWORDS);            
+            System.out.println("name: " + i.getTitle());            
+            
+            //check that all requested objects are contained within search result
+            Helper.checkIncluded(i, INVESTIGATORS_AND_KEYWORDS);
+            
+            //if we get here without exception then all is OK!
+            assertTrue(true);
+                                  
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+    
+    @Test
+    public void keywordSearchInvestigatorsOnly() {    
+        try {  
+            // TODO process result here
+            sessionId = adminPort.loginAdmin(USER1);            
+            if (sessionId == null) assertTrue(false);                        
+            System.out.println("sessionId: " + sessionId);
+                                    
+            //get armstrong investigation
+            List<uk.icat3.client.Investigation> investigations = port.searchByKeywords(sessionId, keywords);        
+            
+            //if no results returned, don't bother do anything else
+            if (investigations.size() == 0) assertTrue(false);
+            
+            uk.icat3.client.Investigation i = port.getInvestigationIncludes(sessionId, investigations.get(0).getId(), INVESTIGATORS_ONLY);            
+            System.out.println("name: " + i.getTitle());            
+            
+            //check that all requested objects are contained within search result
+            Helper.checkIncluded(i, INVESTIGATORS_ONLY);
+            
+            //if we get here without exception then all is OK!
+            assertTrue(true);
+                                  
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+    
+    
+    @Test
+    public void keywordSearchInvestigatorsShiftsAndSamples() {    
+        try {  
+            // TODO process result here
+            sessionId = adminPort.loginAdmin(USER1);            
+            if (sessionId == null) assertTrue(false);                        
+            System.out.println("sessionId: " + sessionId);
+                                    
+            //get armstrong investigation
+            List<uk.icat3.client.Investigation> investigations = port.searchByKeywords(sessionId, keywords);        
+            
+            //if no results returned, don't bother do anything else
+            if (investigations.size() == 0) assertTrue(false);
+            
+            uk.icat3.client.Investigation i = port.getInvestigationIncludes(sessionId, investigations.get(0).getId(), INVESTIGATORS_SHIFTS_AND_SAMPLES);            
+            System.out.println("name: " + i.getTitle());            
+            
+            //check that all requested objects are contained within search result
+            Helper.checkIncluded(i, INVESTIGATORS_SHIFTS_AND_SAMPLES);
+            
+            //if we get here without exception then all is OK!
+            assertTrue(true);
+                                  
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+    
+    @Test
+    public void keywordSearchInvestigatorsShiftsSamplesAndPublications() {    
+        try {  
+            // TODO process result here
+            sessionId = adminPort.loginAdmin(USER1);            
+            if (sessionId == null) assertTrue(false);                        
+            System.out.println("sessionId: " + sessionId);
+                                    
+            //get armstrong investigation
+            List<uk.icat3.client.Investigation> investigations = port.searchByKeywords(sessionId, keywords);        
+            
+            //if no results returned, don't bother do anything else
+            if (investigations.size() == 0) assertTrue(false);
+            
+            uk.icat3.client.Investigation i = port.getInvestigationIncludes(sessionId, investigations.get(0).getId(), INVESTIGATORS_SHIFTS_SAMPLES_AND_PUBLICATIONS);            
+            System.out.println("name: " + i.getTitle());            
+            
+            //check that all requested objects are contained within search result
+            Helper.checkIncluded(i, INVESTIGATORS_SHIFTS_SAMPLES_AND_PUBLICATIONS);
+            
+            //if we get here without exception then all is OK!
+            assertTrue(true);
+                                  
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+    
+    
+    @Test
+    public void keywordSearchKeywordsOnly() {    
+        try {  
+            // TODO process result here
+            sessionId = adminPort.loginAdmin(USER1);            
+            if (sessionId == null) assertTrue(false);                        
+            System.out.println("sessionId: " + sessionId);
+                                    
+            //get armstrong investigation
+            List<uk.icat3.client.Investigation> investigations = port.searchByKeywords(sessionId, keywords);        
+            
+            //if no results returned, don't bother do anything else
+            if (investigations.size() == 0) assertTrue(false);
+            
+            uk.icat3.client.Investigation i = port.getInvestigationIncludes(sessionId, investigations.get(0).getId(), KEYWORDS_ONLY);            
+            System.out.println("name: " + i.getTitle());            
+            
+            //check that all requested objects are contained within search result
+            Helper.checkIncluded(i, KEYWORDS_ONLY);
+            
+            //if we get here without exception then all is OK!
+            assertTrue(true);
+                                  
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+    
+    @Test
+    public void keywordSearchPublicationsOnly() {    
+        try {  
+            // TODO process result here
+            sessionId = adminPort.loginAdmin(USER1);            
+            if (sessionId == null) assertTrue(false);                        
+            System.out.println("sessionId: " + sessionId);
+                                    
+            //get armstrong investigation
+            List<uk.icat3.client.Investigation> investigations = port.searchByKeywords(sessionId, keywords);        
+            
+            //if no results returned, don't bother do anything else
+            if (investigations.size() == 0) assertTrue(false);
+            
+            uk.icat3.client.Investigation i = port.getInvestigationIncludes(sessionId, investigations.get(0).getId(), PUBLICATIONS_ONLY);            
+            System.out.println("name: " + i.getTitle());            
+            
+            //check that all requested objects are contained within search result
+            Helper.checkIncluded(i, PUBLICATIONS_ONLY);
+            
+            //if we get here without exception then all is OK!
+            assertTrue(true);
+                                  
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+    
+    @Test
+    public void keywordSearchSamplesOnly() {    
+        try {  
+            // TODO process result here
+            sessionId = adminPort.loginAdmin(USER1);            
+            if (sessionId == null) assertTrue(false);                        
+            System.out.println("sessionId: " + sessionId);
+                                    
+            //get armstrong investigation
+            List<uk.icat3.client.Investigation> investigations = port.searchByKeywords(sessionId, keywords);        
+            
+            //if no results returned, don't bother do anything else
+            if (investigations.size() == 0) assertTrue(false);
+            
+            uk.icat3.client.Investigation i = port.getInvestigationIncludes(sessionId, investigations.get(0).getId(), SAMPLES_ONLY);            
+            System.out.println("name: " + i.getTitle());            
+            
+            //check that all requested objects are contained within search result
+            Helper.checkIncluded(i, SAMPLES_ONLY);
+            
+            //if we get here without exception then all is OK!
+            assertTrue(true);
+                                  
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+    
 }
