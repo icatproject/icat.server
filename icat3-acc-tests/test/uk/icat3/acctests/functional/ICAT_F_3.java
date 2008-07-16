1a42120102a555edd84a9704f6794bb106af7317
Damian Flannery <d.w.flannery@gmail.com>
2008-07-16 15:46:17 +0000

    Committing boiler plate code

--- icat3-acc-tests/test/uk/icat3/acctests/functional/ICAT_F_3.java
+++ icat3-acc-tests/test/uk/icat3/acctests/functional/ICAT_F_3.java
@@ -5,28 +5,62 @@
 
 package uk.icat3.acctests.functional;
 
+import java.util.ArrayList;
+import java.util.List;
+import javax.xml.ws.BindingProvider;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
+import static uk.icat3.acctests.util.Constants.*;
 import org.junit.Test;
-import static org.junit.Assert.*;
 
 /**
- *
+ *  
  * @author df01
+ * The following unit tests are designed to ensure that correct data is
+ * returned for the various combinations of advanced search criteria
+ * 
  */
 public class ICAT_F_3 {
 
+    private static uk.icat3.client.admin.ICATAdminService adminService = null;
+    private static uk.icat3.client.admin.ICATAdmin adminPort = null;
+    private static uk.icat3.client.ICATService service = null;
+    private static uk.icat3.client.ICAT port = null;        
+    private static java.lang.String sessionId = null;        
+    
     public ICAT_F_3() {
     }
 
     @BeforeClass
     public static void setUpClass() throws Exception {
+        try {    // Call Web Service Operation
+            adminService = new uk.icat3.client.admin.ICATAdminService();
+            adminPort = adminService.getICATAdminPort();
+            
+            ((BindingProvider)adminPort).getRequestContext().put(BindingProvider.USERNAME_PROPERTY, ICAT_ADMIN_USER);
+            ((BindingProvider)adminPort).getRequestContext().put(BindingProvider.PASSWORD_PROPERTY, ICAT_ADMIN_PASSWORD);  
+            
+            service = new uk.icat3.client.ICATService();
+            port = service.getICATPort();
+            
+            sessionId = adminPort.loginAdmin(ISIS_GUARDIAN);
+            
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }   
     }
 
     @AfterClass
     public static void tearDownClass() throws Exception {
+        port.logout(sessionId);
+        sessionId = null;
+        adminService = null;
+        adminPort = null;
+        service = null;
+        port = null;
+        
     }
 
     @Before
@@ -35,12 +69,11 @@ public class ICAT_F_3 {
 
     @After
     public void tearDown() {
+        
     }
 
-    // TODO add test methods here.
-    // The methods must be annotated with annotation @Test. For example:
-    //
-    // @Test
-    // public void hello() {}
+    
+    @Test
+    public void searchAdvancedByRunNumber() {}
 
 }
