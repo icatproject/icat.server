86271906629dd24079488746250f1a8b7a52b80d
Damian Flannery <d.w.flannery@gmail.com>
2009-01-08 11:45:59 +0000

    Added new generic icat xml ingest client

---
+++ icat3-xmlingest-client/build/generated/wsimport/client/uk/icat3/client/InvestigationInclude.java
@@ -0,0 +1,64 @@
+
+package uk.icat3.client;
+
+import javax.xml.bind.annotation.XmlEnum;
+import javax.xml.bind.annotation.XmlType;
+
+
+/**
+ * <p>Java class for investigationInclude.
+ * 
+ * <p>The following schema fragment specifies the expected content contained within this class.
+ * <p>
+ * <pre>
+ * &lt;simpleType name="investigationInclude">
+ *   &lt;restriction base="{http://www.w3.org/2001/XMLSchema}string">
+ *     &lt;enumeration value="INVESTIGATORS_ONLY"/>
+ *     &lt;enumeration value="KEYWORDS_ONLY"/>
+ *     &lt;enumeration value="PUBLICAIIONS_ONLY"/>
+ *     &lt;enumeration value="INVESTIGATORS_AND_KEYWORDS"/>
+ *     &lt;enumeration value="INVESTIGATORS_AND_SHIFTS"/>
+ *     &lt;enumeration value="DATASETS_ONLY"/>
+ *     &lt;enumeration value="DATASETS_AND_DATAFILES"/>
+ *     &lt;enumeration value="DATASETS_DATAFILES_AND_PARAMETERS"/>
+ *     &lt;enumeration value="SAMPLES_ONLY"/>
+ *     &lt;enumeration value="ROLE_ONLY"/>
+ *     &lt;enumeration value="SHIFT_ONLY"/>
+ *     &lt;enumeration value="ALL"/>
+ *     &lt;enumeration value="NONE"/>
+ *     &lt;enumeration value="ALL_EXCEPT_DATASETS_AND_DATAFILES"/>
+ *     &lt;enumeration value="ALL_EXCEPT_DATASETS_DATAFILES_AND_ROLES"/>
+ *   &lt;/restriction>
+ * &lt;/simpleType>
+ * </pre>
+ * 
+ */
+@XmlType(name = "investigationInclude")
+@XmlEnum
+public enum InvestigationInclude {
+
+    INVESTIGATORS_ONLY,
+    KEYWORDS_ONLY,
+    PUBLICAIIONS_ONLY,
+    INVESTIGATORS_AND_KEYWORDS,
+    INVESTIGATORS_AND_SHIFTS,
+    DATASETS_ONLY,
+    DATASETS_AND_DATAFILES,
+    DATASETS_DATAFILES_AND_PARAMETERS,
+    SAMPLES_ONLY,
+    ROLE_ONLY,
+    SHIFT_ONLY,
+    ALL,
+    NONE,
+    ALL_EXCEPT_DATASETS_AND_DATAFILES,
+    ALL_EXCEPT_DATASETS_DATAFILES_AND_ROLES;
+
+    public String value() {
+        return name();
+    }
+
+    public static InvestigationInclude fromValue(String v) {
+        return valueOf(v);
+    }
+
+}
