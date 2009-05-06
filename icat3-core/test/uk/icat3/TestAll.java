7f924f94be2e135c8bee58a17ac2becbbabf1663
Damian Flannery <d.w.flannery@gmail.com>
2009-05-06 15:19:53 +0000

    Updated executable test suites

--- icat3-core/test/uk/icat3/TestAll.java
+++ icat3-core/test/uk/icat3/TestAll.java
@@ -22,7 +22,7 @@ import uk.icat3.datafilemanager.TestDatafile;
 import uk.icat3.datafilemanager.TestDatafileParameter;
 import uk.icat3.datasetmanager.TestDataset;
 import uk.icat3.datasetmanager.TestDatasetParameter;
-import uk.icat3.entity.EntityBaseBean;
+import uk.icat3.investigationmanager.TestEntityBaseBean;
 import uk.icat3.investigationmanager.TestInvestigation;
 import uk.icat3.investigationmanager.TestInvestigator;
 import uk.icat3.investigationmanager.TestKeyword;
@@ -30,10 +30,8 @@ import uk.icat3.investigationmanager.TestManagerUtil;
 import uk.icat3.investigationmanager.TestPublication;
 import uk.icat3.investigationmanager.TestSample;
 import uk.icat3.investigationmanager.TestSampleParameter;
-import uk.icat3.search.TestDatafileSearch;
 import uk.icat3.search.TestDatasetSearch;
 import uk.icat3.search.TestInvalidUser;
-import uk.icat3.search.TestInvestigationListSearch;
 import uk.icat3.search.TestInvestigationSearch;
 import uk.icat3.search.TestKeywordSearch;
 import uk.icat3.security.TestGateKeeperAdminDatafile;
@@ -54,82 +52,67 @@ import uk.icat3.security.TestGateKeeperIcatAdminInvestigation;
 import uk.icat3.security.TestGateKeeperReaderDatafile;
 import uk.icat3.security.TestGateKeeperReaderDataset;
 import uk.icat3.security.TestGateKeeperReaderInvestigation;
-import uk.icat3.security.TestGateKeeperSuperUserInvestigation;
 import uk.icat3.security.TestGateKeeperUpdaterDatafile;
 import uk.icat3.security.TestGateKeeperUpdaterDataset;
 import uk.icat3.security.TestGateKeeperUpdaterInvestigation;
-import uk.icat3.util.ExecuteDatabaseScript;
 
 @RunWith(Suite.class)
 @Suite.SuiteClasses({
-    
-   /* TestInvalidUser.class,
-            
-            TestDatafileSearch.class,
-            TestDatasetSearch.class,
-            TestInvestigationSearch.class,
-            TestInvestigationListSearch.class,
-             
-            TestSample.class,
-            TestKeyword.class,
-            TestPublication.class,
-            TestInvestigator.class,
-            TestInvestigation.class,
-            TestSampleParameter.class,
-            // TestManagerUtil.class,
-            //  EntityBaseBean.class,
-            
-            TestDataset.class,
-            TestDatasetParameter.class,
-            
-            TestDatafile.class,
-            TestDatafileParameter.class,
-            
-            TestKeywordSearch.class,
-            */
-            
-            TestGateKeeperAdminInvestigation.class,
-            TestGateKeeperCreatorInvestigation.class,
-            TestGateKeeperDeleterInvestigation.class,
-            TestGateKeeperDownloaderInvestigation.class,
-            TestGateKeeperIcatAdminInvestigation.class,
-            TestGateKeeperReaderInvestigation.class,
-            TestGateKeeperUpdaterInvestigation.class,
-            TestGateKeeperSuperUserInvestigation.class,
-            
-            TestGateKeeperAdminDataset.class,
-            TestGateKeeperCreatorDataset.class,
-            TestGateKeeperDeleterDataset.class,
-            TestGateKeeperDownloaderDataset.class,
-            TestGateKeeperIcatAdminDataset.class,
-            TestGateKeeperReaderDataset.class,
-            TestGateKeeperUpdaterDataset.class,
-            
-            TestGateKeeperAdminDatafile.class,
-            TestGateKeeperCreatorDatafile.class,
-            TestGateKeeperDeleterDatafile.class,
-            TestGateKeeperDownloaderDatafile.class,
-            TestGateKeeperIcatAdminDatafile.class,
-            TestGateKeeperReaderDatafile.class,
-            TestGateKeeperUpdaterDatafile.class
+    TestInvestigation.class,
+    TestInvestigator.class,
+    TestKeyword.class,
+
+    TestPublication.class,
+    TestSample.class,
+    TestSampleParameter.class,
+    TestDataset.class,
+    TestDatasetParameter.class,
+    TestDatafile.class,
+    TestDatafileParameter.class,
+    TestEntityBaseBean.class,
+    TestManagerUtil.class,
+
+    TestDatasetSearch.class,
+    TestInvalidUser.class,
+    TestKeywordSearch.class,
+
+    /*
+     * Need modification...
+     * TestDatafileSearch.class,
+     */
+
+   // TestInvestigationListSearch.class,  //*
+    TestInvestigationSearch.class,      //*
+
+    TestGateKeeperAdminInvestigation.class,
+    TestGateKeeperCreatorInvestigation.class,
+    TestGateKeeperDeleterInvestigation.class,
+    TestGateKeeperDownloaderInvestigation.class,
+    TestGateKeeperIcatAdminInvestigation.class,
+    TestGateKeeperReaderInvestigation.class,
+    TestGateKeeperUpdaterInvestigation.class,
+
+    TestGateKeeperAdminDataset.class,
+    TestGateKeeperCreatorDataset.class,
+    TestGateKeeperDeleterDataset.class,
+    TestGateKeeperDownloaderDataset.class,
+    TestGateKeeperIcatAdminDataset.class,
+    TestGateKeeperReaderDataset.class,
+    TestGateKeeperUpdaterDataset.class,
+
+    TestGateKeeperAdminDatafile.class,
+    TestGateKeeperCreatorDatafile.class,
+    TestGateKeeperDeleterDatafile.class,
+    TestGateKeeperDownloaderDatafile.class,
+    TestGateKeeperIcatAdminDatafile.class,
+    TestGateKeeperReaderDatafile.class,
+    TestGateKeeperUpdaterDatafile.class
             
 })
         public class TestAll {
     
     
     public static Test suite() {
-        
-        //icat unit test (glens)
-        ExecuteDatabaseScript script = new ExecuteDatabaseScript("jdbc:oracle:thin:@(DESCRIPTION=(ADDRESS=(HOST=elektra.dl.ac.uk)(PROTOCOL=tcp)(PORT=1521))(CONNECT_DATA=(SID=minerva2)))", "icat_unittest", "s4nfr4n");
-        
-        //icat scratch
-        //ExecuteDatabaseScript script = new ExecuteDatabaseScript("jdbc:oracle:thin:@(DESCRIPTION=(ADDRESS=(HOST=elektra.dl.ac.uk)(PROTOCOL=tcp)(PORT=1521))(CONNECT_DATA=(SID=minerva2)))", "icat_scratch", "c1sco");
-        
-        script.execute("database/ICAT3API_DropTables[v1].sql", ";");
-        script.execute("database/ICAT3API_CreateSchema[v1].sql", ";");
-        script.execute("database/ICAT3API_InsertListTables[v1].sql", ";");
-        script.execute("database/ICAT3API_JUNIT_InsertTestData[v1].sql", ";");
-        
         return new JUnit4TestAdapter(TestAll.class);
     }
 }
