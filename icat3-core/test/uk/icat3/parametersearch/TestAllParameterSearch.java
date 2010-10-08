082d93d377729bc77977f34bf3890c2728d39c1a
Najor Cruzcruz <najorcruzcruz@gmail.com>
2010-10-08 08:42:07 +0000

    · Problem solved about Datatime parameter (Issue 37)
    · New test added

--- icat3-core/test/uk/icat3/parametersearch/TestAllParameterSearch.java
+++ icat3-core/test/uk/icat3/parametersearch/TestAllParameterSearch.java
@@ -12,7 +12,9 @@ import junit.framework.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import uk.icat3.parametersearch.datafile.DatafileTest;
+import uk.icat3.parametersearch.datafile.ValueTypeDatafileTest;
 import uk.icat3.parametersearch.dataset.DatasetTest;
+import uk.icat3.parametersearch.dataset.ValueTypeDatasetTest;
 import uk.icat3.parametersearch.exception.DatafileExceptionTest;
 import uk.icat3.parametersearch.exception.DatasetExceptionTest;
 import uk.icat3.parametersearch.exception.InvestigationExceptionTest;
@@ -20,7 +22,9 @@ import uk.icat3.parametersearch.exception.SampleExceptionTest;
 import uk.icat3.parametersearch.investigation.ListComparatorTest;
 import uk.icat3.parametersearch.investigation.ListParameterTest;
 import uk.icat3.parametersearch.investigation.OperableTest;
+import uk.icat3.parametersearch.investigation.ValueTypeInvestigationTest;
 import uk.icat3.parametersearch.sample.SampleTest;
+import uk.icat3.parametersearch.sample.ValueTypeSampleTest;
 
 /**
  *
@@ -29,11 +33,15 @@ import uk.icat3.parametersearch.sample.SampleTest;
 @RunWith(Suite.class)
 @Suite.SuiteClasses({
     DatafileTest.class,
+    ValueTypeDatafileTest.class,
     DatasetTest.class,
+    ValueTypeDatasetTest.class,
     SampleTest.class,
+    ValueTypeSampleTest.class,
     ListParameterTest.class,
     ListComparatorTest.class,
     OperableTest.class,
+    ValueTypeInvestigationTest.class,
     InvestigationExceptionTest.class,
     DatafileExceptionTest.class,
     DatasetExceptionTest.class,
