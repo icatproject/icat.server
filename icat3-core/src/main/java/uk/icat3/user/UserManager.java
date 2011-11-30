0cb2a9b482b0461e4286de85a68ba632f2cf78ae
Steve Fisher <dr.s.m.fisher@gmail.com>
2011-11-30 13:21:53 +0000

    This is an intermediate checkin in for preparing the plugin authentication - it will not currently compile!

--- icat3-core/src/main/java/uk/icat3/user/UserManager.java
+++ icat3-core/src/main/java/uk/icat3/user/UserManager.java
@@ -9,41 +9,12 @@ import org.apache.log4j.Logger;
 import uk.icat3.exceptions.NoSuchUserException;
 import uk.icat3.exceptions.SessionException;
 
-
-/*
- * UserManager.java
- *
- * Created on 20 February 2007, 15:53
- *
- * Added UserManager class that will act as the user database implementation
- * and will be used directly by icat3.  In fact, this class will delegate all
- * method calls to a facility specific implementation of the User.java interface
- * (e.g. to ISISUser.java - which will provide all the necessary work to
- * retrieve the desired information from the ISIS user database and present it
- * in a form digestable for icat3).
- *
- * @author df01
- * @version 1.0
- */
 public class UserManager implements User {
     
-    // Global class logger
     static Logger log = Logger.getLogger(UserManager.class);
     
     private User user;
-    //private EntityManager manager;
-    //private boolean isEntityManager = false;
-    
-    /** Creates a new instance of UserManager */
-    public UserManager(String className) throws SessionException {
-        user = (uk.icat3.user.User) createObject(className);
-    }
-    
-     /** Creates a new instance of UserManager, uses default user implementation*/
-    public UserManager(EntityManager manager) throws SessionException {
-        user = (uk.icat3.user.User) createObject("uk.icat3.userdefault.facility.DefaultUser",manager);
-    }
-    
+ 
     /** Creates a new instance of UserManager with entity manager */
     public UserManager(String className, EntityManager manager) throws SessionException {
         user = (uk.icat3.user.User) createObject(className, manager);
@@ -68,28 +39,16 @@ public class UserManager implements User {
     public UserDetails getUserDetails(String sessionId, String user) throws SessionException, NoSuchUserException {
         return this.user.getUserDetails(sessionId, user);
     }
-    
-    private static Object createObject(String className) {
-        Object object = null;
-        try {
-            Class classDefinition = Class.forName(className);
-            object = classDefinition.newInstance();
-            log.trace("Object: " + object.toString());
-        } catch (Exception e) {
-            log.error(e);
-        }
-        return object;
-    }
-    
+       
     private static Object createObject(String  className, EntityManager manager) {
-        Constructor entityManagerConstructor = null;
+        Constructor<?> entityManagerConstructor = null;
         Object object = null;
         
         try {
-            Class[] entityManagerArgaClass = new Class[] {EntityManager.class};
+            Class<?>[] entityManagerArgaClass = new Class[] {EntityManager.class};
             Object[] inArgs = new Object[] {manager};
             
-            Class classDefinition = Class.forName(className);
+            Class<?> classDefinition = Class.forName(className);
             
             entityManagerConstructor = classDefinition.getConstructor(entityManagerArgaClass);
             
