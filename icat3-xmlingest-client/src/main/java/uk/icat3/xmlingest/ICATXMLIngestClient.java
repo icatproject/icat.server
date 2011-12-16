5c77c85deef79371bdd44a08763644c76f64a692
Steve Fisher <dr.s.m.fisher@gmail.com>
2011-12-16 12:40:18 +0000

    Clean up a bit - but it still needs tests doing properly. Ingest is probably broken at the moment because the automatic provision of a facility is no longer handled.

--- icat3-xmlingest-client/src/main/java/uk/icat3/xmlingest/ICATXMLIngestClient.java
+++ icat3-xmlingest-client/src/main/java/uk/icat3/xmlingest/ICATXMLIngestClient.java
@@ -6,22 +6,71 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.URL;
-import java.util.Enumeration;
 import java.util.List;
 import java.util.Properties;
-import java.util.PropertyResourceBundle;
-import java.util.ResourceBundle;
 
 import javax.xml.namespace.QName;
 
 import uk.icat3.client.ICAT;
 import uk.icat3.client.ICATService;
 
-/**
- * 
- * @author df01
- */
 public class ICATXMLIngestClient {
+
+	public static List<Long> ingestInvestigation(String user, String password, String xml, Properties properties) {
+
+		ICAT icatPort = null;
+		List<Long> ids = null;
+
+		try {
+			final String urlString = properties.getProperty("icat.url");
+			if (urlString == null) {
+				System.err.println("icat.url property not set in icatclient.properties");
+				System.exit(1);
+			}
+			final URL icatURL = new URL(properties.getProperty("icat.url") + "/ICATService/ICAT?wsdl");
+			icatPort = new ICATService(icatURL, new QName("client.icat3.uk", "ICATService")).getICATPort();
+
+			System.out.println("Logging in...");
+			final String sessionId = icatPort.login(user, password);
+
+			System.out.println("SessionId = " + sessionId);
+
+			System.out.println("Ingesting metadata...");
+			ids = icatPort.ingestMetadata(sessionId, xml);
+
+			System.out.println("Logging out...");
+			icatPort.logout(sessionId);
+
+			return ids;
+		} catch (final Exception ex) {
+			ex.printStackTrace();
+			return null;
+		}
+	}
+
+	public static void main(String[] args) throws IOException {
+
+		if (args == null || args.length < 3) {
+			System.err.println("[ERROR] - Incorrect number of arguments supplied");
+			printHelp();
+			System.exit(-1);
+		}
+
+		final String username = args[0];
+		final String password = args[1];
+
+		final String buffer = readXMLFile(args[2]);
+
+		final InputStream fis = ICATXMLIngestClient.class.getResourceAsStream("/icatclient.properties");
+		final Properties properties = new Properties();
+		properties.load(fis);
+
+		final List<Long> ids = ingestInvestigation(username, password, buffer, properties);
+
+		for (final Long id : ids) {
+			System.out.println("Returned : " + id);
+		}
+	}
 
 	public static void printHelp() {
 		System.out.println();
@@ -30,76 +79,19 @@ public class ICATXMLIngestClient {
 	}
 
 	public static String readXMLFile(String _filename) {
-		StringBuffer _buffer = new StringBuffer();
+		final StringBuffer _buffer = new StringBuffer();
 		try {
-			BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(_filename)));
+			final BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(_filename)));
 			String line = "";
 			while ((line = br.readLine()) != null) {
 				_buffer.append(line);
 			}// end while
-		} catch (Exception e) {
+		} catch (final Exception e) {
 			System.err.println("[ERROR] - Supplied filename does not exist or cannot be read");
 			printHelp();
 			System.exit(-1);
 		}// end try/catch
 
 		return _buffer.toString();
-	}
-
-
-	public static void main(String[] args) throws IOException {
-
-		if ((args == null) || (args.length < 3)) {
-			System.err.println("[ERROR] - Incorrect number of arguments supplied");
-			printHelp();
-			System.exit(-1);
-		}
-
-		String username = args[0];
-		String password = args[1];
-
-		String buffer = readXMLFile(args[2]);
-
-		InputStream fis = ICATXMLIngestClient.class.getResourceAsStream("/icatclient.properties");
-		Properties properties = new Properties();
-		properties.load(fis);
-
-		List<Long> ids = ingestInvestigation(username, password, buffer, properties);
-
-		for (Long id : ids)
-			System.out.println("Returned : " + id);
-	}
-
-	public static List<Long> ingestInvestigation(String user, String password, String xml, Properties properties) {
-
-		ICAT icatPort = null;
-		List<Long> ids = null;
-
-		try {
-			String urlString = properties.getProperty("icat.url");
-			if (urlString == null) {
-				System.err.println("icat.url property not set in icatclient.properties");
-				System.exit(1);
-			}
-			URL icatURL = new URL(properties.getProperty("icat.url") + "/ICATService/ICAT?wsdl");
-			icatPort = new ICATService(icatURL, new QName("client.icat3.uk", "ICATService")).getICATPort();
-
-			System.out.println("Logging in...");
-			String sessionId = icatPort.login(user, password);
-
-			System.out.println("SessionId = " + sessionId);
-
-			// ingest here
-			System.out.println("Ingesting metadata...");
-			ids = icatPort.ingestMetadata(sessionId, xml);
-
-			System.out.println("Logging out...");
-			icatPort.logout(sessionId);
-
-			return ids;
-		} catch (Exception ex) {
-			ex.printStackTrace();
-			return null;
-		}
 	}
 }
