87dcb75ba55c4dde4daea903b175ef86e8913b08
Steve Fisher <dr.s.m.fisher@gmail.com>
2012-01-13 15:34:30 +0000

    Improved POM and fix to logging text

--- icat3-core/src/main/java/uk/icat3/entity/InputDatafile.java
+++ icat3-core/src/main/java/uk/icat3/entity/InputDatafile.java
@@ -63,7 +63,7 @@ public class InputDatafile extends EntityBaseBean implements Serializable {
 	}
 
 	public void beforeMarshal(Marshaller source) {
-		logger.trace("Marshalling Application for " + includes);
+		logger.trace("Marshalling InputDatafile for " + includes);
 		if (!this.includes.contains(Job.class)) {
 			this.job = null;
 		}
