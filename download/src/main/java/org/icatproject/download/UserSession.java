f7cf19539ef47f9f85eae1a8221cf72e09ac6d43
Steve Fisher <dr.s.m.fisher@gmail.com>
2012-05-15 16:58:53 +0000

    Not needed in 4.1

--- download/src/main/java/org/icatproject/download/UserSession.java
+++
@@ -1,9 +0,0 @@
-package org.icatproject.download;
-
-import org.icatproject.core.IcatException;
-
-public interface UserSession {
-
-    public String getUserIdFromSessionId(String sessionId) throws IcatException;
-
-}
