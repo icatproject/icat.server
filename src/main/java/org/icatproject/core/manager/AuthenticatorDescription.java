0a4b07d96c885fc81fc1fbc7e87295e716b411be
Rolf Krahl <rolf.krahl@helmholtz-berlin.de>
2015-12-18 15:52:22 +0100

    Parse the JSON authenticator description into an AuthenticatorDescription.

--- src/main/java/org/icatproject/core/manager/AuthenticatorDescription.java
+++ src/main/java/org/icatproject/core/manager/AuthenticatorDescription.java
@@ -1,7 +1,13 @@
 package org.icatproject.core.manager;
 
+import java.io.StringReader;
 import java.util.ArrayList;
 import java.util.List;
+
+import javax.json.Json;
+import javax.json.JsonArray;
+import javax.json.JsonObject;
+import javax.json.JsonReader;
 
 import org.icatproject.core.manager.AuthenticatorCredentialKey;
 
@@ -9,15 +15,23 @@ public class AuthenticatorDescription {
 
     private List<AuthenticatorCredentialKey> keys;
 
-    public AuthenticatorDescription(String description) {
-	this.keys = new ArrayList<AuthenticatorCredentialKey>();
-	AuthenticatorCredentialKey k = new AuthenticatorCredentialKey();
-	k.setName("username");
-	this.keys.add(k);
-	k = new AuthenticatorCredentialKey();
-	k.setName("password");
-	k.setHide(true);
-	this.keys.add(k);
+    public AuthenticatorDescription(String description) 
+	throws RuntimeException {
+	try (JsonReader jr = Json.createReader(new StringReader(description))) {
+	    this.keys = new ArrayList<AuthenticatorCredentialKey>();
+	    JsonArray jkeys = jr.readObject().getJsonArray("keys");
+	    for (int i = 0; i < jkeys.size(); i++) {
+		AuthenticatorCredentialKey key = new AuthenticatorCredentialKey();
+		JsonObject jkey = jkeys.getJsonObject(i);
+		key.setName(jkey.getString("name"));
+		try {
+		    key.setHide(jkey.getBoolean("hide"));
+		} catch (NullPointerException e) {
+		    // hide is not set.
+		}
+		this.keys.add(key);
+	    }
+	}
     }
 
     public List<AuthenticatorCredentialKey> getKeys() {
