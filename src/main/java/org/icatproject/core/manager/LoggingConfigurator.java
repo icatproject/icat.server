cd9a24494c81e44c3fa59b66577010f0bac26200
Steve Fisher <dr.s.m.fisher@gmail.com>
2015-12-23 10:33:21 +0000

    Tidying up a bit

--- src/main/java/org/icatproject/core/manager/LoggingConfigurator.java
+++ src/main/java/org/icatproject/core/manager/LoggingConfigurator.java
@@ -1,13 +1,13 @@
 package org.icatproject.core.manager;
 
 import java.io.File;
-import java.io.FileInputStream;
-import java.util.Properties;
 
 import javax.annotation.PostConstruct;
 import javax.ejb.Singleton;
 import javax.ejb.Startup;
 
+import org.icatproject.utils.CheckedProperties;
+import org.icatproject.utils.CheckedProperties.CheckedPropertyException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -23,39 +23,38 @@ public class LoggingConfigurator {
 
 	@PostConstruct
 	private void init() {
-		File f = new File("icat.properties");
-		Properties props = new Properties();
+
 		try {
-			props.load(new FileInputStream(f));
-		} catch (Exception e) {
-			String msg = "Problem with " + f.getAbsolutePath() + "  " + e.getMessage();
-			throw new IllegalStateException(msg);
-		}
+			CheckedProperties props = new CheckedProperties();
+			props.loadFromFile("icat.properties");
 
-		String path = props.getProperty("logback.xml");
-		if (path != null) {
-			f = new File(path);
-			if (!f.exists()) {
-				String msg = "logback.xml file " + f.getAbsolutePath() + " specified in icat.properties not found";
-				throw new IllegalStateException(msg);
+			File f = null;
+			if (props.has("logback.xml")) {
+				f = props.getFile("logback.xml");
+				if (!f.exists()) {
+					String msg = "logback.xml file " + f.getAbsolutePath() + " specified in " + f + " not found";
+					throw new IllegalStateException(msg);
+				}
+				LoggerContext context = (LoggerContext) LoggerFactory.getILoggerFactory();
+				try {
+					JoranConfigurator configurator = new JoranConfigurator();
+					configurator.setContext((Context) LoggerFactory.getILoggerFactory());
+					context.reset();
+					configurator.doConfigure(f);
+				} catch (JoranException je) {
+					// StatusPrinter will handle this
+				}
+				StatusPrinter.printInCaseOfErrorsOrWarnings(context);
 			}
-			LoggerContext context = (LoggerContext) LoggerFactory.getILoggerFactory();
-			try {
-				JoranConfigurator configurator = new JoranConfigurator();
-				configurator.setContext((Context) LoggerFactory.getILoggerFactory());
-				context.reset();
-				configurator.doConfigure(f);
-			} catch (JoranException je) {
-				// StatusPrinter will handle this
-			}
-			StatusPrinter.printInCaseOfErrorsOrWarnings(context);
-		}
 
-		Logger logger = LoggerFactory.getLogger(LoggingConfigurator.class);
-		if (path != null) {
-			logger.info("Logging configuration read from " + path);
-		} else {
-			logger.info("Using logback default configuration");
+			Logger logger = LoggerFactory.getLogger(LoggingConfigurator.class);
+			if (f != null) {
+				logger.info("Logging configuration read from " + f);
+			} else {
+				logger.info("Using logback default configuration");
+			}
+		} catch (CheckedPropertyException e) {
+			throw new IllegalStateException(e.getMessage());
 		}
 	}
 }
