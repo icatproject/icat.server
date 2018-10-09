031c03b7b926d6b8d7b9644e2b58906bdec57861
Rolf Krahl <rolf.krahl@helmholtz-berlin.de>
2018-10-09 13:27:31 +0200

    Bump version to 4.10.0-SNAPSHOT and update release notes.

--- src/test/java/org/icatproject/integration/TestWS.java
+++ src/test/java/org/icatproject/integration/TestWS.java
@@ -68,7 +68,7 @@ import org.junit.Test;
  */
 public class TestWS {
 
-	private static final String version = "4.9.";
+	private static final String version = "4.10.";
 	private static Random random;
 	private static WSession session;
 
