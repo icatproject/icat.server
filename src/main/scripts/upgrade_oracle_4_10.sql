0278be16f116d57e1cf811966dc15b7c5846c808
Rolf Krahl <rolf.krahl@helmholtz-berlin.de>
2018-10-09 17:04:39 +0200

    Add schema upgrade scripts along with instructions to use them.

---
+++ src/main/scripts/upgrade_oracle_4_10.sql
@@ -0,0 +1,14 @@
+alter table INSTRUMENT add PID VARCHAR2(255);
+alter table PARAMETERTYPE add PID VARCHAR2(255);
+alter table SAMPLE add PID VARCHAR2(255);
+alter table SHIFT add (
+	INSTRUMENT_ID NUMBER(19),
+	constraint FK_SHIFT_INSTRUMENT_ID
+	foreign key (INSTRUMENT_ID) references INSTRUMENT (ID)
+);
+alter table USER_ add (
+	GIVENNAME VARCHAR2(255),
+	FAMILYNAME VARCHAR2(255),
+	AFFILIATION VARCHAR2(255)
+);
+exit
