4e4c85ed7eaa02e167ccc0a18786fef1cbacffdc
Stuart Pullinger <stuartpullinger@gmail.com>
2019-03-18 00:02:37 +0000

    Correct column type for Oracle DB. Fixes #224.

--- src/main/scripts/upgrade_oracle_4_10.sql
+++ src/main/scripts/upgrade_oracle_4_10.sql
@@ -7,7 +7,7 @@ alter table SHIFT add (
 	foreign key (INSTRUMENT_ID) references INSTRUMENT (ID)
 );
 alter table STUDY add PID VARCHAR2(255);
-alter table STUDY add ENDDATE DATETIME;
+alter table STUDY add ENDDATE TIMESTAMP;
 alter table USER_ add (
 	GIVENNAME VARCHAR2(255),
 	FAMILYNAME VARCHAR2(255),
