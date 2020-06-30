0026580db066050aafee522c84b037fd51cefbbc
Emil Junker <emil.junker@outlook.com>
2020-06-30 09:24:59 +0200

    remove relation from Technique to Facility

--- src/main/java/org/icatproject/core/entity/Technique.java
+++ src/main/java/org/icatproject/core/entity/Technique.java
@@ -4,21 +4,14 @@ import java.io.Serializable;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import javax.persistence.UniqueConstraint;
 
 @Comment("Represents an experimental technique.")
 @SuppressWarnings("serial")
 @Entity
-@Table(uniqueConstraints = { @UniqueConstraint(columnNames = { "FACILITY_ID", "NAME" }) })
+@Table(uniqueConstraints = { @UniqueConstraint(columnNames = { "NAME" }) })
 public class Technique extends EntityBaseBean implements Serializable {
-
-	@JoinColumn(name = "FACILITY_ID", nullable = false)
-	@ManyToOne(fetch = FetchType.LAZY)
-	private Facility facility;
 
 	@Comment("A short name for the technique")
 	@Column(name = "NAME", nullable = false)
@@ -30,10 +23,6 @@ public class Technique extends EntityBaseBean implements Serializable {
 	@Comment("An informal description")
 	private String description;
 
-	public Facility getFacility() {
-		return facility;
-	}
-
 	public String getName() {
 		return name;
 	}
@@ -44,10 +33,6 @@ public class Technique extends EntityBaseBean implements Serializable {
 
 	public String getDescription() {
 		return description;
-	}
-
-	public void setFacility(Facility facility) {
-		this.facility = facility;
 	}
 
 	public void setName(String name) {
