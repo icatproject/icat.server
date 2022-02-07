1e51c9829cd110e1b1280062995733e6b864211c
kevinphippsstfc <kevinphippsstfc@users.noreply.github.com>
2022-02-07 13:43:49 +0000

    List imports individually to match other entities

--- src/main/java/org/icatproject/core/entity/FacilityCycle.java
+++ src/main/java/org/icatproject/core/entity/FacilityCycle.java
@@ -5,7 +5,17 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
-import javax.persistence.*;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.persistence.UniqueConstraint;
 
 @Comment("An operating cycle within a facility")
 @SuppressWarnings("serial")
