f09b64ed4f461532fe529510aa122374bb9c1987
kevinphippsstfc <kevinphippsstfc@users.noreply.github.com>
2022-02-07 13:48:26 +0000

    Remove unused imports

--- src/main/java/org/icatproject/core/entity/InvestigationFacilityCycle.java
+++ src/main/java/org/icatproject/core/entity/InvestigationFacilityCycle.java
@@ -2,8 +2,6 @@ package org.icatproject.core.entity;
 
 import java.io.Serializable;
 
-import javax.json.stream.JsonGenerator;
-import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
