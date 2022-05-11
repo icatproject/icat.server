e8c7547e9cd909faa7cdc3f072e465d002bd045f
Alan Kyffin <alan.kyffin@stfc.ac.uk>
2022-05-11 17:46:06 +0100

    Update the expected version in tests

--- src/test/java/org/icatproject/integration/TestRS.java
+++ src/test/java/org/icatproject/integration/TestRS.java
@@ -500,7 +500,7 @@ public class TestRS {
 	@Test
 	public void testVersion() throws Exception {
 		ICAT icat = new ICAT(System.getProperty("serverUrl"));
-		assertTrue(icat.getVersion().startsWith("5."));
+		assertTrue(icat.getVersion().startsWith("6."));
 	}
 
 	@Test
