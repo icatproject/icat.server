e5b0733c6a8bcb73bb4ee8fd6f1f6f3da731f2fa
Alan Kyffin <alan.kyffin@stfc.ac.uk>
2025-07-10 10:22:59 +0100

    Clarify that EntityManager in SessionManager is for a different persistence context

--- src/main/java/org/icatproject/core/manager/SessionManager.java
+++ src/main/java/org/icatproject/core/manager/SessionManager.java
@@ -37,8 +37,10 @@ public class SessionManager {
 	@Resource
 	UserTransaction userTransaction;
 
+	// This EntityManager is for a different persistence context from the rest of icat.server that only contains the
+	// Session entity.
 	@PersistenceContext(unitName = "session")
-	EntityManager entityManager;
+	EntityManager sessionEntityManager;
 
 	@EJB
 	PropertyHandler propertyHandler;
@@ -56,7 +58,7 @@ public class SessionManager {
 	@Schedule(hour = "*")
 	public void removeExpiredSessions() {
 		try {
-			int n = entityManager.createNamedQuery(Session.DELETE_EXPIRED).executeUpdate();
+			int n = sessionEntityManager.createNamedQuery(Session.DELETE_EXPIRED).executeUpdate();
 			logger.debug(n + " sessions were removed");
 		} catch (Throwable e) {
 			logger.error(e.getClass() + " " + e.getMessage());
@@ -69,13 +71,12 @@ public class SessionManager {
 		return session.getRemainingMinutes();
 	}
 
-
 	private Session getSession(String sessionId) throws IcatException {
 		Session session = null;
 		if (sessionId == null || sessionId.equals("")) {
 			throw new IcatException(IcatException.IcatExceptionType.SESSION, "Session Id cannot be null or empty.");
 		}
-		session = (Session) entityManager.find(Session.class, sessionId);
+		session = (Session) sessionEntityManager.find(Session.class, sessionId);
 		if (session == null) {
 			throw new IcatException(IcatException.IcatExceptionType.SESSION,
 					"Unable to find user by sessionid: " + sessionId);
@@ -97,7 +98,7 @@ public class SessionManager {
 
 	public boolean isLoggedIn(String userName) {
 		logger.debug("isLoggedIn for user " + userName);
-		return entityManager.createNamedQuery(Session.ISLOGGEDIN, Long.class).setParameter("userName", userName).getSingleResult() > 0;
+		return sessionEntityManager.createNamedQuery(Session.ISLOGGEDIN, Long.class).setParameter("userName", userName).getSingleResult() > 0;
 	}
 
 	public String login(String userName, int lifetimeMinutes, String ip) throws IcatException {
@@ -106,8 +107,8 @@ public class SessionManager {
 			userTransaction.begin();
 			try {
 				long startMillis = log ? System.currentTimeMillis() : 0;
-				entityManager.persist(session);
-				entityManager.flush();
+				sessionEntityManager.persist(session);
+				sessionEntityManager.flush();
 				userTransaction.commit();
 				String result = session.getId();
 				logger.debug("Session " + result + " persisted.");
@@ -146,8 +147,8 @@ public class SessionManager {
 			try {
 				long startMillis = log ? System.currentTimeMillis() : 0;
 				Session session = getSession(sessionId);
-				entityManager.remove(session);
-				entityManager.flush();
+				sessionEntityManager.remove(session);
+				sessionEntityManager.flush();
 				userTransaction.commit();
 				logger.debug("Session {} removed.", session.getId());
 				if (logRequests.contains(CallType.SESSION)) {
@@ -191,7 +192,7 @@ public class SessionManager {
 				long startMillis = log ? System.currentTimeMillis() : 0;
 				Session session = getSession(sessionId);
 				session.refresh(lifetimeMinutes);
-				entityManager.flush();
+				sessionEntityManager.flush();
 				userTransaction.commit();
 				logger.debug("Session {} refreshed.", session.getId());
 				if (logRequests.contains(CallType.SESSION)) {
