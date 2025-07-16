4f2ff3b9f9be18cc300fab50789543ce172749da
Rolf Krahl <rolf.krahl@helmholtz-berlin.de>
2025-07-16 23:12:20 +0200

    Add a simple check searching for the DataPublication

--- src/test/java/org/icatproject/integration/TestRS.java
+++ src/test/java/org/icatproject/integration/TestRS.java
@@ -406,6 +406,22 @@ public class TestRS {
 		 */
 		JsonArray j_response = search(session, "SELECT j from Job j", 1);
 		collector.checkThat(j_response.getJsonObject(0).containsKey("Job"), is(true));
+
+		/*
+		 * Expected: <[{"DataPublication":{"id":6,"createId":"simple/root","createTime":
+		 * "2025-07-16T22:54:23.000+02:00","modId":"simple/root","modTime":
+		 * "2025-07-16T22:54:23.000+02:00","dates":[],"description":
+		 * "We provide the first 65535 integers [...] a one dimensional integer array.",
+		 * "fundingReferences":[],"pid":"DOI:00.0815/pub-00027","publicationDate":
+		 * "2022-10-31T00:00:00.000+01:00","relatedItems":[],"subject":
+		 * "integer sequence; OEIS; On-Line Encyclopedia of Integer Sequences",
+		 * "title":"Data from OEIS sequence A000027","users":[]}}]>
+		 */
+		JsonArray datapub_response = search(session, "SELECT d FROM DataPublication d", 1);
+		collector.checkThat(datapub_response.getJsonObject(0).containsKey("DataPublication"), is(true));
+		collector.checkThat(
+				datapub_response.getJsonObject(0).getJsonObject("DataPublication").getJsonString("title").getString(),
+				is("Data from OEIS sequence A000027"));
 	}
 
 	@Test
